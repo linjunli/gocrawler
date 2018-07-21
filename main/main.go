bd68c276e77c75eff1c797fd49d9e2aeb3aae523
lilili <894266047@qq.com>
2018-07-21 09:38:23 +0800

    Update main.go

--- main/main.go
+++ main/main.go
@@ -6,20 +6,12 @@ import (
 )
 
 func main() {
-	//var simplerTest engine.Trending
-	//
-	//simplerTest.Run(
-	//	engine.RequestForGithub{
-	//		URL: "https://github.com/trending",
-	//		ParseFunc:github.ParseForGithub,
-	//	},
-	//)
-
-	var yztTest engine.Yzt
-	yztTest.Run(
+	var simplerTest engine.Trending
+	
+	simplerTest.Run(
 		engine.RequestForGithub{
-			URL:"https://www.yingzt.com/invest/normalAppListB?deviceId=140034125209534465",
-			ParseFunc:yzt.ParseForYzt,
+			URL: "https://github.com/trending",
+			ParseFunc:github.ParseForGithub,
 		},
 	)
 }
